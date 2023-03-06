d48ee43bf35046963143d317a8ac80d07424f921
KobzarAnatolii <TohaPirat@gmail.com>
2023-03-06 12:53:07 +0200

    refactoring on the hooks

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,86 +1,14 @@
 import { FeedbackOptions } from 'components/Feedback/FeedbackOptions/FeedbackOptions';
 import { Statistics } from 'components/Feedback/Statistics/Statistics';
 import { Section } from './Section/Section';
-import { useState } from 'react';
-
-// export class oldFeedback extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   addFeedback = value => {
-//     this.setState(prevState => {
-//       return {
-//         [value]: prevState[value] + 1,
-//       };
-//     });
-//   };
-
-//   resetStatistics = value => {
-//     if (value) {
-//       this.setState({
-//         good: 0,
-//         neutral: 0,
-//         bad: 0,
-//       });
-//     }
-//   };
-
-//   countTotalFeedback() {
-//     const { good, neutral, bad } = this.state;
-
-//     return good + neutral + bad;
-//   }
-
-//   countPositiveFeedbackPercentage() {
-//     const { good } = this.state;
-
-//     return (good / this.countTotalFeedback()) * 100;
-//   }
-
-//   componentDidMount() {
-//     const feedback = JSON.parse(localStorage.getItem('feedback'));
-
-//     if (feedback) {
-//       this.setState(feedback);
-//     }
-//   }
-
-//   componentDidUpdate(prevState) {
-//     if (this.state !== prevState) {
-//       localStorage.setItem('feedback', JSON.stringify(this.state));
-//     }
-//   }
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     return (
-//       <Section title={'Please leave feeedback'}>
-//         <FeedbackOptions
-//           options={this.state}
-//           onLeaveFeedback={event => {
-//             this.addFeedback(event.target.value);
-//           }}
-//         />
-//         <Statistics
-//           good={good}
-//           neutral={neutral}
-//           bad={bad}
-//           total={this.countTotalFeedback()}
-//           positivePercentage={this.countPositiveFeedbackPercentage()}
-//           resetStatistics={this.resetStatistics}
-//         />
-//       </Section>
-//     );
-//   }
-// }
+import { useLocalStorage } from 'hooks/useLocalStorage';
 
 export const Feedback = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [good, setGood] = useLocalStorage('good', 0);
+
+  const [neutral, setNeutral] = useLocalStorage('neutral', 0);
+
+  const [bad, setBad] = useLocalStorage('bad', 0);
 
   const addFeedback = value => {
     switch (value) {
